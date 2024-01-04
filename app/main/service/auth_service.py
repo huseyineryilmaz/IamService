c74c93973bb12a07c100d848a8a7de936da91c61
Huseyin Eryilmaz <dev.huseyineryilmaz@gmail.com>
2024-01-05 00:28:35 +0300

    refactor

--- app/main/service/auth_service.py
+++ app/main/service/auth_service.py
@@ -1,5 +1,5 @@
-
 from ..model.user_model import User
+from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity
 import bcrypt
 
 def register(request):
@@ -12,21 +12,26 @@ def register(request):
         new_user = User(email=email, first_name=first_name, last_name=last_name, password=password)
         new_user.save()
     except Exception as e:
-        return {'success': False, 'Exception': e.args}
-    return {'success': True, 'message': 'User created.'}
+        return {'success': False, 'Exception': e.args}, 400
+    return {'success': True, 'message': 'User created.'}, 200
 
 def login(request):
     try:
         data = request.get_json()
         email = data['email']
         password = data['password'].encode('utf-8')
-        user = User.objects(email=email)
-        if user and bcrypt.checkpw(password, user.password.encode('utf-8')):
-            return {'success': True, 'message': 'Login succeed.'}
+        login_user = User.objects(email__exact=email)
+        if login_user and bcrypt.checkpw(password, login_user.first().password.encode('utf-8')):
+            return {'success': True,
+                    'message': 'Login succeed.',
+                    'access_token': create_access_token(identity=email),
+                    'refresh_token': create_refresh_token(identity=email)
+                }, 200
         else:
-            return {'success': False, 'message': 'Login failed.'}
+            return {'success': False, 'message': 'Login failed.'}, 401
     except Exception as e:
-        return {'success': False, 'Exception': e.args}
+        return {'success': False, 'Exception': e.args}, 401
+
 def refresh():
     current_user = get_jwt_identity()
     return {'access_token': create_access_token(identity=current_user)}, 200
--- app/main/service/user_service.py
+++ app/main/service/user_service.py
@@ -1,10 +1,10 @@
 from ..model.user_model import User
 
 def get_user(email):
-    user = User.objects(email=email).to_json()
+    user = User.objects(email__exact=email).first().to_json()
     if user:
-        return {'success': True, 'user': user}
-    return {'success': False, 'message': 'user does not exist'}
+        return {'success': True, 'user': user}, 200
+    return {'success': False, 'message': 'user does not exist'}, 200
 
 def get_all_users():
-    return {'success': True, 'users': User.objects.to_json()}
+    return {'success': True, 'users': User.objects.to_json()}, 200
